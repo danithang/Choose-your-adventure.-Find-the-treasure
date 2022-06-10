fbb8a28a800f339db330224e6bdbcc63899317a3
Astra Berry <astraberry82@gmail.com>
2022-06-10 23:03:16 +0000

    Added comments

--- main.py
+++ main.py
@@ -26,25 +26,27 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 
-choice1 = input("Which way to the secret treasure 'right' or 'left'? \n Choose wisely or face the consequences. \n").lower()
+choice1 = input("Which way to the secret treasure 'right' or 'left'? \nChoose wisely or face the consequences. \n").lower()
 if choice1 == "right":
   print("Good choice, you can move on")
-  
-  choice2 = input("Would you like to swim across immediately or wait for 1 hour for a boat? \n  Please type 'swim' or 'boat'. \n").lower()
+  #nested if else statement for choice2, if got passed choice1
+  choice2 = input("Would you like to swim across immediately or wait for 1 hour for a boat? \nPlease type 'swim' or 'boat'. \n").lower()
   if choice2 == "boat":
     print("Nice!  You are one step closer to the treasure. ")
     
-    choice3 = input("One more choice, three doors, choose wisely. \n Which door 'red', 'yellow', or 'blue'? \n").lower()
+    #nested if, elif, else statement for choice3
+    choice3 = input("One more choice, three doors, choose wisely. \nWhich door 'red', 'yellow', or 'blue'? \n").lower()
     if choice3 == ("blue"):
         print("Yay!  You got the treasure.  Congrats!")
     elif choice3 == ("red"):
-        print("You just got burned alive in a fiery pit! \n Better luck next time!")
+        print("You just got burned alive in a firey pit! \nBetter luck next time!")
     else:
         print("You chose door yellow \n and you fell into shark infested water and died!  \n Sucks for you!")
-  
+  #choice2 else statement if failed choice2
   else:
     print("Sorry, you drowned.  Game Over!")
 
+#choice1 else statement, if choose wrong on choice1 can't go to other choices
 else:
   print("Sorry, you fell off a cliff.  Game Over!")
 
